4830dbbe5d329f3b135e1df695b23f9806085108
FARED HOMS <68755503+muhammed-fared@users.noreply.github.com>
2020-11-06 00:10:25 +0300

    Update Main_File.java

--- odevler/vize_1/Main_File.java
+++ odevler/vize_1/Main_File.java
@@ -2,10 +2,10 @@ public static void main(String[] args) {
         
 //       
         
-       System.out.println(MyUtils2.ucgenCevre(15,5,4));
-	System.out.println(MyUtils2.ucgenAlan(6,10,4));
-        System.out.println(MyUtils2.cokgenCevre(5, 4, 5, 4));
-        System.out.println(MyUtils2.cokgenAlan(7, 5));
+       System.out.println(MyUtils.ucgenCevre(15,5,4));
+	System.out.println(MyUtils.ucgenAlan(6,10,4));
+        System.out.println(MyUtils.cokgenCevre(5, 4, 5, 4));
+        System.out.println(MyUtils.cokgenAlan(7, 5));
 	double[] arr = new double[500];
         int j = 0;
 	 for(int i = -100; i <= 100; i++)
@@ -14,7 +14,7 @@ public static void main(String[] args) {
             j++;
             
         }
-         double [] sonuc = MyUtils2.yap(arr);
+         double [] sonuc = MyUtils.yap(arr);
           for(int i = 0; i < sonuc.length; i++)
         {
             System.out.println(sonuc[i]);
